537aba6a69209ae3d78ef92dfb7b0d6f5b3a7c81
kunalkirar <kunalkirar111@gmail.com>
2021-01-30 13:44:05 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { green, pink } from '@material-ui/core/colors';
+import Avatar from '@material-ui/core/Avatar';
+import FolderIcon from '@material-ui/icons/Folder';
+import PageviewIcon from '@material-ui/icons/Pageview';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 
-export default function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  pink: {
+    color: theme.palette.getContrastText(pink[500]),
+    backgroundColor: pink[500],
+  },
+  green: {
+    color: '#fff',
+    backgroundColor: green[500],
+  },
+}));
+
+export default function IconAvatars() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className={classes.root}>
+      <Avatar>
+        <FolderIcon />
+      </Avatar>
+      <Avatar className={classes.pink}>
+        <PageviewIcon />
+      </Avatar>
+      <Avatar className={classes.green}>
+        <AssignmentIcon />
+      </Avatar>
+      <Avatar variant="square" className={classes.square}>
+  N
+</Avatar>
+<Avatar variant="rounded" className={classes.rounded}>
+  <AssignmentIcon />
+</Avatar>
+<Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" className={classes.small} />
+<Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+<Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" className={classes.large} />
     </div>
   );
 }
